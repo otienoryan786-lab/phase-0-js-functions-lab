edf859b1e147db15c140fc2d4d3c93b2e126d606
otienoryan786-lab <otienoryan786@gmail.com>
2026-09-23 16:23:48 +0300

    git commit -m "Add budgeting utility functions"

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+function calculateTax(amount) {
+    return amount * 0.1; // Assuming a tax rate of 10%
+}
 
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+    return Math.max(num1, num2);
+}
 
+function isPalindrome(word) {
+    const reversedWord = cleanedWord.split('').reverse().join('');
+    return cleanedWord === reversedWord;
+}
 
-// This is required for the test to function properly  
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountAmount = (originalPrice * discountPercentage) / 100;
+    return originalPrice - discountAmount;
+}
+
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
